f04843b24ca54e4b47674dc68a154941f624da19
sanjusingh21 <48952789+sanjusingh21@users.noreply.github.com>
2026-07-21 11:47:47 +0530

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
+      <head />
       <body>
         <AppContextProvider>
           <ClientLayout>
